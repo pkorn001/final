146f24f6a016dbfe2c8e2ee6fa791d72978451ce
Kittikorn Leelawanich <pkorn003@gmail.com>
2019-05-02 20:31:02 +0700

    Update Irenderable.java

--- src/render/Irenderable.java
+++ src/render/Irenderable.java
@@ -2,7 +2,7 @@ package render;
 
 import java.awt.Graphics2D;
 
-public interface Renderable {
+public interface Irenderable {
 	public void draw(Graphics2D g2d);
 	public boolean IsVisible();
 	public int getZ();
