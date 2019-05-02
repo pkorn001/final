cfca8e149b32e6dc8f8c953aa5e6604493ec0421
Sitthibudhi Mongkolsri <sitthiput.m@gmail.com>
2019-05-02 21:57:45 +0700

    Edit boomerange's movement

--- src/hero/action/Boomerange.java
+++ src/hero/action/Boomerange.java
@@ -7,12 +7,18 @@ public class Boomerange extends Hitbox {
 	
 	private Position returnPoint;
 	private Position originPoint;
+	private Position heroPoint;
+	private Hitbox hero;
+	private boolean isReturn = false;
 	private boolean doneFirst = false;
 	
 	public Boomerange(Position position, int xSpeed, int ySpeed) {
 		super(position, 1, 1, xSpeed, ySpeed);
+		heroPoint = new Position(position.getX()-1, position.getY());
 		originPoint = new Position(position.getX(), position.getY());
 		returnPoint = new Position(position.getX() + 8, position.getY()/2 + 4);
+		hero = new Hitbox(heroPoint, 0) {
+		};
 	}
 	
 	public void move() {
@@ -22,12 +28,12 @@ public class Boomerange extends Hitbox {
 			this.setC(new Position(this.C.getX()+2, this.C.getY()+1));
 			this.setD(new Position(this.D.getX()+2, this.D.getY()+1));
 		}else if(this.C.getX() >= returnPoint.getX() && this.C.getY() > returnPoint.getY()) {
+			doneFirst = true;
 			this.setA(new Position(this.A.getX()+1, this.A.getY()-2));
 			this.setB(new Position(this.B.getX()+1, this.B.getY()-2));
 			this.setC(new Position(this.C.getX()+1, this.C.getY()-2));
 			this.setD(new Position(this.D.getX()+1, this.D.getY()-2));
 		}else if(this.C.getX() >= returnPoint.getX() && this.C.getY() > originPoint.getY()){
-			doneFirst = true;
 			this.setA(new Position(this.A.getX()-1, this.A.getY()-2));
 			this.setB(new Position(this.B.getX()-1, this.B.getY()-2));
 			this.setC(new Position(this.C.getX()-1, this.C.getY()-2));
@@ -38,5 +44,16 @@ public class Boomerange extends Hitbox {
 			this.setC(new Position(this.C.getX()-2, this.C.getY()));
 			this.setD(new Position(this.D.getX()-2, this.D.getY()));	
 		}
+		if(this.collide(hero)) {
+			setReturn(true);
+		}
+	}
+
+	public boolean isReturn() {
+		return isReturn;
+	}
+
+	public void setReturn(boolean isReturn) {
+		this.isReturn = isReturn;
 	}
 }
