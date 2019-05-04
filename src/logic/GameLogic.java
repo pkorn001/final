ed9d4ad79a63fe954e4ff33f95e8a1325fabaade
Kittikorn Leelawanich <pkorn003@gmail.com>
2019-05-05 02:23:26 +0700

    Update GameLogic.java
    Create BossAttackGen()

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -1,24 +1,22 @@
 package logic;
 
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import boss.Boss;
 import boss.BossAttack;
 import boss.ParriedBall;
-import java.util.Random;
-import hero.base.Hero;
-import obstacle.Monster;
-import obstacle.ObstacleBox;
-import scene.GameScreen;
-import java.awt.event.KeyEvent;
 import hero.base.Assassin;
 import hero.base.Boomeranger;
 import hero.base.Hero;
 import hero.base.Mage;
 import hero.base.Swordman;
-
-public class GameLogic {
+import obstacle.Monster;
+import obstacle.ObstacleBox;
+import render.RenderableHolder;
+import scene.GameScreen;
 
 public class GameLogic {
 	
@@ -47,8 +45,386 @@ public class GameLogic {
 		screen = new GameScreen(1920, 1080);
 		this.speedFactor = 1;
 		
+		RenderableHolder.getInstance().add(hero);
+		RenderableHolder.getInstance().add(boss);
+		everything.add(hero);
+		everything.add(boss);
 	}
+	
+	public void BossAttackGen() {
+		Random attackPattern = new Random();
+		Random attackType = new Random();
+		Random monsterType = new Random();
+	
+		List<Integer> list = new ArrayList<>();
+		List<Integer> list2 = new ArrayList<>();
+		for (int e = 0; e < 10; e++) {
+			list.add(e);
+		}
+		for (int e = 0; e < 4; e++) {
+			list2.add(e);
+		}
+		
+		if ((boss.getBossHp() <= 100)) {
+			switch (attackPattern.nextInt(2)) {
+	
+			case 1:
+				for (int j = 0; j < 5; j++) {
+					int i = attackPattern.nextInt(list.size());
+					int k = i;
+					list.remove(k);
+					
+					if (i == 0) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 100),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 1) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 100),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 2) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 250),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 3) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 250),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 4) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 5) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 6) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 7) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 8) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 7) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+					try {
+						wait(200);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+	
+			case 2:
+				for (int i = 0; i < 5; i++) {
+						/*
+						 * 0 = parried 1 = boss_fireball 2 = monster
+						 */
+						if (attackType.nextInt(3) == 0) {
+							ParriedBall e = (ParriedBall) boss.create(new Position(boss.getB().getX(),hero.getA().getY()+100),0,-1,-50, 0);
+							boss_Attack.add(e);
+							everything.add(e);
+							RenderableHolder.getInstance().add(e);
+						} else if (attackType.nextInt(3) == 1) {
+							BossAttack e = (BossAttack) boss.create(new Position(boss.getB().getX(),hero.getB().getY()-100),1,-1,-50, 0);
+							boss_Attack.add(e);
+							everything.add(e);
+							RenderableHolder.getInstance().add(e);
+						} else if (attackType.nextInt(3) == 2) {
+							Monster e = (Monster) boss.create(new Position(boss.getB().getX(),hero.getA().getY()+100),2,monsterType.nextInt(6)+1,-50, 0);
+							boss_Attack.add(e);
+							everything.add(e);
+							RenderableHolder.getInstance().add(e);
+						}
+						try {
+							wait(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
 
+			default:
+				for (int j = 0; j < 3; j++) {
+					int i = attackPattern.nextInt(list2.size());
+					int k = i;
+					list.remove(k);
+	
+					if (i == 0) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 100),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 150),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 200),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+					}
+	
+					else if (i == 1) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 150),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 200),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+					}
+	
+					else if (i == 2) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 200),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 150),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+					}
+	
+					else if (i == 3) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 250),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 150),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 200),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+	
+					}
+					try {
+						wait(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} else {
+	
+			switch (attackPattern.nextInt(1)) {
+			case 1:
+				for (int j = 0; j < 5; j++) {
+					int i = attackPattern.nextInt(list.size());
+					int k = i;
+					list.remove(k);
+					
+					if (i == 0) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 100),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 1) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 100),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 2) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 250),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 3) {
+						ParriedBall e = (ParriedBall) boss.create(new Position(1920, 250),0,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+	
+					else if (i == 4) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 5) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 6) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 7) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 8) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					} else if (i == 7) {
+						BossAttack e = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e);
+						everything.add(e);
+						RenderableHolder.getInstance().add(e);
+					}
+					try {
+						wait(200);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+	
+			default:
+				for (int j = 0; j < 3; j++) {
+					int i = attackPattern.nextInt(list2.size());
+					int k = i;
+					list.remove(k);
+	
+					if (i == 0) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 100),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 150),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 200),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+					}
+	
+					else if (i == 1) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 150),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 200),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+					}
+	
+					else if (i == 2) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 200),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 150),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 250),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+					}
+	
+					else if (i == 3) {
+						ParriedBall e1 = (ParriedBall) boss.create(new Position(1920, 250),0,-1,-50, 0);
+						BossAttack e2 = (BossAttack) boss.create(new Position(1920, 100),1,-1,-50, 0);
+						BossAttack e3 = (BossAttack) boss.create(new Position(1920, 150),1,-1,-50, 0);
+						BossAttack e4 = (BossAttack) boss.create(new Position(1920, 200),1,-1,-50, 0);
+						boss_Attack.add(e1);
+						boss_Attack.add(e2);
+						boss_Attack.add(e3);
+						boss_Attack.add(e4);
+						everything.add(e1);
+						everything.add(e2);
+						everything.add(e3);
+						everything.add(e4);
+						RenderableHolder.getInstance().add(e1);
+						RenderableHolder.getInstance().add(e2);
+						RenderableHolder.getInstance().add(e3);
+						RenderableHolder.getInstance().add(e4);
+	
+					}
+					try {
+						wait(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+	
+	
 	public static void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_SPACE && !jump) {
 			setJump(true);
