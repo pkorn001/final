66b6cc16c9b1b5df12bb3dd1c1fe4e6e1224fe63
Sitthibudhi Mongkolsri <sitthiput.m@gmail.com>
2019-05-05 03:36:53 +0700

    recode this class

--- src/scene/GameScreen.java
+++ src/scene/GameScreen.java
@@ -1,39 +1,22 @@
 package scene;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.WritableImage;
 
 public class GameScreen extends Canvas {
 
-	private Background topBackground;
-	private Background bottomBackground;
-	
-	public GameScreen(double width, double height){
-		super(width, height);
-		this.setVisible(true);
-		
-		Pane root = new Pane();
-		VBox topBox = new VBox(width);  //top part, start from hero's line till the max height
-		topBox.setBackground(null);
-		
-		VBox bottomBox = new VBox(width);
-		
-		HBox score = new HBox();  //score of the game
-		score.setAlignment(Pos.TOP_LEFT);
-		score.setPadding(new Insets(.5));
-		
-		HBox logo = new HBox();   //game's design
-		logo.setAlignment(Pos.TOP_RIGHT);
-		logo.setPadding(new Insets(.5));
-		
-		topBox.getChildren().addAll(score, logo);
-		
-		root.getChildren().addAll(topBox, bottomBox);
+	public GameScreen(double time, int stage){
+		switch(stage) {
+		case(0):
+			break;
+		case(1):
+			break;
+		case(2):
+			break;
+		case(3):
+			break;
+		case(4):
+			break;
+		}
 	}
 }
