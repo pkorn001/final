253358ec9c3eef95d574e6035b81a6f04a1a0f5f
Sitthibudhi Mongkolsri <sitthiput.m@gmail.com>
2019-05-05 04:09:33 +0700

    edit GameScreen

--- src/scene/GameScreen.java
+++ src/scene/GameScreen.java
@@ -1,21 +1,30 @@
 package scene;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 
 public class GameScreen extends Canvas {
 
+	private Image background;
+	private Canvas canvas = new Canvas(1600, 900);
+	
 	public GameScreen(double time, int stage){
 		switch(stage) {
 		case(0):
+			
 			break;
 		case(1):
+			
 			break;
 		case(2):
+			
 			break;
 		case(3):
+			
 			break;
 		case(4):
+			
 			break;
 		}
 	}
