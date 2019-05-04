c58f971c9bf66a6a9830f924d9e45d6d3475ba7c
Sitthibudhi Mongkolsri <sitthiput.m@gmail.com>
2019-05-05 00:44:50 +0700

    Create input logic

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -1,5 +1,66 @@
 package logic;
 
+import java.awt.event.KeyEvent;
+import hero.base.Assassin;
+import hero.base.Boomeranger;
+import hero.base.Hero;
+import hero.base.Mage;
+import hero.base.Swordman;
+
 public class GameLogic {
 
+	private Hero hero;
+	private static boolean jump;
+	private static boolean attack;
+
+	public static void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_SPACE && !jump) {
+			setJump(true);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_A && !attack) {
+			setAttack(true);
+		}
+	}
+
+	public static void keyRelease(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			setJump(false);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			setAttack(false);
+		}
+	}
+
+	public void updateState() {
+		if (isJump()) {
+			hero.jump();
+		}
+		if (isAttack()) {
+			if (hero instanceof Mage) {
+				((Mage) hero).attack();
+			} else if (hero instanceof Boomeranger) {
+				((Boomeranger) hero).attack();
+			} else if (hero instanceof Swordman) {
+				((Swordman) hero).attack();
+			} else if (hero instanceof Assassin) {
+				((Assassin) hero).attack();
+			}
+		}
+	}
+
+	public static boolean isJump() {
+		return jump;
+	}
+
+	public static void setJump(boolean jump) {
+		GameLogic.jump = jump;
+	}
+
+	public static boolean isAttack() {
+		return attack;
+	}
+
+	public static void setAttack(boolean attack) {
+		GameLogic.attack = attack;
+	}
 }
