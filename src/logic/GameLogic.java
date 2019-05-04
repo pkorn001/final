7133d08e26acc6c4af014c5dd5b041dc2d88d926
Kittikorn Leelawanich <pkorn003@gmail.com>
2019-05-05 01:21:46 +0700

    Update GameLogic.java

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -1,7 +1,12 @@
 package logic;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import boss.Boss;
+import boss.BossAttack;
+import boss.ParriedBall;
+import java.util.Random;
 import hero.base.Hero;
 import obstacle.Monster;
 import obstacle.ObstacleBox;
@@ -11,12 +16,30 @@ public class GameLogic {
 	
 	private static GameLogic instance = new GameLogic();
 	protected Hero hero;
+	protected Boss boss;
 	protected GameScreen screen;
 	protected static List<Monster> monsters;
 	protected static List<ObstacleBox> obstacleBoxes;
-	protected static List<Object> bossAttack;
+	protected static List<Object> boss_Attack;
 	protected static List<Hitbox> everything;
 	protected static double speedFactor;
+	
+	public GameLogic() {
+		hero = new Hero(new Position(20.00,0.00),0) {
+			
+			@Override
+			public void updateScore(Monster monster) {
+				// TODO Auto-generated method stub
+				setScore( getScore() + monster.getMonsterPoint());
+			}};
+		
+		boss = new Boss(new Position(1600,500), 0, 0);
+		screen = new GameScreen(1920, 1080);
+		this.speedFactor = 1;
+		
+	}
+		
+	
 	
 	
 }
