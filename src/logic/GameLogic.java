6809d8ffd953f156e68e3692a1d2fcf193dfe806
Kittikorn Leelawanich <pkorn003@gmail.com>
2019-05-05 00:04:16 +0700

    Update GameLogic.java

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -1,5 +1,22 @@
 package logic;
 
-public class GameLogic {
+import java.util.List;
 
+import hero.base.Hero;
+import obstacle.Monster;
+import obstacle.ObstacleBox;
+import scene.GameScreen;
+
+public class GameLogic {
+	
+	private static GameLogic instance = new GameLogic();
+	protected Hero hero;
+	protected GameScreen screen;
+	protected static List<Monster> monsters;
+	protected static List<ObstacleBox> obstacleBoxes;
+	protected static List<Object> bossAttack;
+	protected static List<Hitbox> everything;
+	protected static double speedFactor;
+	
+	
 }
