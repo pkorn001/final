1878976604b5afeaee1a976c8e058ee2902cc673
Sitthibudhi Mongkolsri <sitthiput.m@gmail.com>
2019-05-04 19:48:41 +0700

    create input class

--- src/input/InputUtility.java
+++ src/input/InputUtility.java
@@ -1,6 +1,44 @@
 package input;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class InputUtility {
+	
+	private static boolean space = false;
+	private static boolean attack = false;
 
+	public static boolean isSpace() {
+		return space;
+	}
+
+	public static boolean isAttack() {
+		return attack;
+	}
+
+	public static void setAttack(boolean attack) {
+		InputUtility.attack = attack;
+	}
+
+	public static void setSpace(boolean space) {
+		InputUtility.space = space;
+	}
+	
+	public static void keyPressed(KeyEvent e) {
+		if (e.getCode() == KeyCode.SPACE && space == false) {
+			InputUtility.setSpace(true);
+		}
+		if(e.getCode() == KeyCode.A && attack == false) {
+			InputUtility.setAttack(true);
+		}
+	}
+	
+	public static void keyRelease(KeyEvent e) {
+		if (e.getCode() == KeyCode.SPACE) {
+			InputUtility.setSpace(false);
+		}
+		if (e.getCode() == KeyCode.A) {
+			InputUtility.setAttack(false);
+		}
+	}
 }
