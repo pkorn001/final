9d9066f27efafdcaba96223c90a3ab3e7ab9ce4c
Sitthibudhi Mongkolsri <sitthiput.m@gmail.com>
2019-05-05 16:36:54 +0700

    test main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,26 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		Button btn = new Button("Hello world");
+		StackPane root = new StackPane(); root.getChildren().add(btn);
+		Scene scene = new Scene(root, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("MyJavaFX"); // Set the stage title primaryStage.setScene(scene); // Place the scene primaryStage.show();
+		primaryStage.show();
+	}
 
 	public static void main(String[] args) {
-
+		launch(args);
 	}
+
 }
