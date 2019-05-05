ef68388ac7b28478a825741fac22d15c8cadc672
Sitthibudhi Mongkolsri <sitthiput.m@gmail.com>
2019-05-06 01:17:52 +0700

    create Score&Logo

---
+++ src/scene/Logo.java
@@ -0,0 +1,28 @@
+package scene;
+
+import hero.base.Hero;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import render.Resource;
+
+public class Logo {
+
+	public Logo() {
+		
+		StackPane root = new StackPane();
+		
+		Image logo = Resource.Logo;
+		
+		Label bg = new Label();
+		bg.setGraphic(new ImageView(logo));
+		bg.setAlignment(Pos.TOP_RIGHT);
+		bg.setPadding(new Insets(.10));
+		
+		root.getChildren().add(bg);
+		
+	}
+}
--- src/scene/Score.java
+++ src/scene/Score.java
@@ -1,10 +1,5 @@
 package scene;
 
-import java.util.Stack;
-
-import javax.swing.JLabel;
-
-import hero.base.Hero;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
@@ -12,7 +7,7 @@ import javafx.scene.layout.StackPane;
 
 public class Score {
 
-	public Score(int score) {
+	public Score(int score) { //get input from Hero.getScore()
 		
 		StackPane root = new StackPane();
 		
