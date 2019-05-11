dbe49095c9206d9955f3dbae469709e0cc8f8842
Kittikorn Leelawanich <pkorn003@gmail.com>
2019-05-12 00:32:48 +0700

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,28 +8,26 @@ import javafx.stage.Stage;
 import logic.GameLogic;
 import render.RenderableHolder;
 import scene.GameScreen;
-import scene.StartScreen;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
 		// TODO Auto-generated method stub
-		StartScreen start = new StartScreen();
 		StackPane root = new StackPane();
-		 Scene scene = new Scene(root);
-		 primaryStage.setScene(scene);
-		 primaryStage.setTitle("LITTLE HERO");
-		 GameScreen gameScreen = new GameScreen();
-		 root.getChildren().add(gameScreen);
-		 primaryStage.show();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("LITTLE HERO");
+		GameScreen gameScreen = new GameScreen();
+		root.getChildren().add(gameScreen);
+		primaryStage.show();
 		 
 		 
 		 new AnimationTimer() {
 			 @Override
 			 public void handle(long now) {
 			    // TODO Add another canvas update
-				 long time = (now - System.nanoTime())/1000000000;
+				long time = 1;
 				RenderableHolder.getInstance().update();
 				GameLogic.logicUpdate(time);
 				gameScreen.paintComponent();
@@ -40,5 +38,4 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 }
